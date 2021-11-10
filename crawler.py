e5300a7f4a8dcf54054b4447f2c4f8805c7df1d9
Acesarsilva <accs2@cin.ufpe.br>
2021-11-10 08:02:27 -0300

    new model and vocabulary

--- crawler.py
+++ crawler.py
@@ -2,8 +2,7 @@ import pandas as pd
 import util
 import pickle
 import nltk
-import socket
-import urllib
+import csv
 from bs4 import BeautifulSoup
 
 def main():
@@ -15,6 +14,10 @@ def main():
     border = [(x,0) for x in start_links]
     model = pickle.load(open('model_perceptron', 'rb'))
     n_links = 1000
+    count = 0
+    f = open('smartphone_links.csv', 'w')
+    writer = csv.writer(f)
+    writer.writerow("smartphone_links")
     while(n_links > 0 and border):
         url = border.pop(0)[0]
         if (url not in downloaded_url):
@@ -25,11 +28,17 @@ def main():
                 #border.sort(key=lambda x:x[1], reverse=True)
                 data = util.pre_processor_model_page(BeautifulSoup(html,'lxml').get_text(),stopwords_en,stopwords_br)
                 pred = model.predict(data)
+                count += pred
+                if(pred == 1):
+                    writer.writerow(url)
                 downloaded_url.append(url)
                 n_links -= 1
             except:
                 print('Bad Download: ', url)
                 border.append((url,0))
+    
+    f.close()
+    return count
 
 if __name__ == "__main__":
     main()
