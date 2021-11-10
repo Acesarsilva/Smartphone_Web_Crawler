054d99f21567380bf8a4a7ca2bf66b5164449f9d
Acesarsilva <accs2@cin.ufpe.br>
2021-11-10 05:26:54 -0300

    Crawler 100% finalizado

--- crawler.py
+++ crawler.py
@@ -1,23 +1,35 @@
 import pandas as pd
 import util
+import pickle
+import nltk
+import socket
+import urllib
+from bs4 import BeautifulSoup
 
 def main():
+    nltk.download('stopwords')
+    stopwords_br = nltk.corpus.stopwords.words('portuguese')
+    stopwords_en = nltk.corpus.stopwords.words('english')
     start_links = pd.read_csv('start_links.csv')['links'].tolist()
     downloaded_url = []
-    border = start_links
-    while(border):
-        url = border.pop(0)
+    border = [(x,0) for x in start_links]
+    model = pickle.load(open('model_perceptron', 'rb'))
+    n_links = 1000
+    while(n_links > 0 and border):
+        url = border.pop(0)[0]
         if (url not in downloaded_url):
             try:
                 html = util.download_url(url)
-                #call html classifier
                 links = util.get_links(html,url)
                 border.extend(links)
+                #border.sort(key=lambda x:x[1], reverse=True)
+                data = util.pre_processor_model_page(BeautifulSoup(html,'lxml').get_text(),stopwords_en,stopwords_br)
+                pred = model.predict(data)
                 downloaded_url.append(url)
-                print(downloaded_url)
+                n_links -= 1
             except:
                 print('Bad Download: ', url)
-                border.append(url)
+                border.append((url,0))
 
 if __name__ == "__main__":
     main()
--- util.py
+++ util.py
@@ -1,19 +1,42 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
+import unicodedata
 
 def download_url(url):
-    return urlopen(url).read().decode('utf-8')
+    return urlopen(url,timeout=1.0).read().decode('utf-8')
 
 def get_links(html,url):
     links = []
     soup = BeautifulSoup(html,'lxml')
     for link in soup.find_all('a'):
-        print(link.text)
         link = link.get('href')
         if(link and link != url and link[0:5] == 'https'):
-            links.append(link)
+            links.append((link, ranker(link)))
     
     return links
 
 def ranker(url):
-    return 0
+    domain_words = {"smartphone","celular", "smartphones", "celulares", "samsung", "apple","motorola","xiaomi", "android", "ios", "galaxy", "moto", "lenovo", "tela", "zenfone", "lg", "telefone","asus","camera","core","mobile","phone","phones"}
+    counter = 0
+    for word in domain_words:
+        if url.lower().find(word) != -1:
+            counter += 1
+    return counter
+
+def pre_processor_model_page(txt,stopwords_en,stopwords_br):
+
+    txt_nfkd = ' '.join(unicodedata.normalize('NFKD', txt).lower().replace('_',' ').replace('!',' ').replace('<',' ').replace('>',' ').replace("'"," ").replace('/',' ').replace('|',' ').replace('?',' ').replace('"',' ').replace('=',' ').replace(';',' ').replace(':',' ').replace('+',' ').replace('-',' ').replace('-',' ').replace('.',' ').replace(',',' ').replace('@',' ').replace('#',' ').replace('$',' ').replace('%',' ').replace('&',' ').replace('*',' ').replace('(',' ').replace(')',' ').replace('[',' ').replace(']',' ').replace('{',' '). replace('}',' ').split()) 
+    txt_remove_accents = txt_nfkd.encode('ASCII', 'ignore').decode('ascii')
+
+    txt_backup = " ".join([word for word in str(txt_remove_accents).split() if (word not in stopwords_br and len(word) >= 3)])
+    txt_remove_stopword = " ".join([word for word in str(txt_backup).split() if (word not in stopwords_en and len(word) >= 3)])
+
+    text_final = " ".join([word for word in str(txt_remove_stopword).split() if (word.isalpha())])
+
+    text_bow = CountVectorizer()
+    data = text_bow.fit_transform([text_final])
+
+    df_bow = pd.DataFrame(data.toarray(),columns=text_bow.get_feature_names())
+    return df_bow
