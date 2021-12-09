cae555198e0b3db1d129756aad5a7c9a23991a81
Acesarsilva <accs2@cin.ufpe.br>
2021-12-09 16:08:13 -0300

    criando interface de texto

--- search.py
+++ search.py
@@ -2,11 +2,34 @@ from util import spearman, kendal_tau
 
 
 def main():
-    #rank1 = [1,2,3,4,5,6,7,8,9,10]
-    #rank2 = [3,1,2,5,4,9,6,7,10,8]
-    rank1 = [1,2,3,4,5]
-    rank2 = [3,1,2,5,4]
-    return kendal_tau(rank1,rank2)
+    run = True
+    while(run):
+        on_query = True
+        word_query = []
+        while(on_query):
+            print("Campos: [geral, memoria, tela, ram, preço, marca]")
+            camp = input("Campo que deseja consultar: ")
+            value = input("Valor que deseja neste campo: ")
+            words = value.split()
+            if(camp != "geral"):
+                words = [(word + '.' + camp).lower() for word in words]
+            
+            word_query.extend(words)
+            end_query = input("Deseja adicionar mais algo a esta consulta? [Y/n]")
+            if(end_query == "n"):
+                on_query = False
+        
+        print("Realizando consulta.....")
+        rank = query(word_query)
+        print("Resultado encontrado: ", rank)
+        end_run = input("Deseja fazer mais uma consulta? [Y/n]")
+        if(end_run == "n"):
+            run = False
+
+    return ("Processo Finalizado")
+
+def query(words):
+    return words
 
 
 if __name__ == "__main__":
